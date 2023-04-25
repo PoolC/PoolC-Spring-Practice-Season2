b61e08f0f4577ab92e55856928cd08d1a20b760d
bc-kim <bc.kim@worksmobile.com>
2023-04-25 22:18:19 +0900

    Initial Commit

---
+++ src/main/java/org/poolc/springpractice/PoolCSpringPracticeSeason2Application.java
@@ -0,0 +1,13 @@
+package org.poolc.springpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolCSpringPracticeSeason2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoolCSpringPracticeSeason2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/poolc/springpractice/PoolCSpringPracticeSeason2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.poolc.springpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoolCSpringPracticeSeason2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
