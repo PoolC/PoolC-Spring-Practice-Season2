6316abb3679f93e62a81b1d42436e4ef5f13bb13
becooq81 <becooq81@gmail.com>
2023-05-09 20:20:32 +0900

    feat: added annotation

--- src/main/java/org/poolc/springpractice/repository/UserRepository.java
+++ src/main/java/org/poolc/springpractice/repository/UserRepository.java
@@ -2,6 +2,8 @@ package org.poolc.springpractice.repository;
 
 import org.poolc.springpractice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
