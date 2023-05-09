cb5bc2686271aab5976fc2c11cde036a31647b6e
becooq81 <becooq81@gmail.com>
2023-05-09 17:45:42 +0900

    feat: added user request payload

--- src/main/java/org/poolc/springpractice/payload/request/user/UserRequest.java
+++ src/main/java/org/poolc/springpractice/payload/request/user/UserRequest.java
@@ -1,2 +1,34 @@
-package org.poolc.springpractice.payload.request.user;public class UserRequest {
+package org.poolc.springpractice.payload.request.user;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Getter
+@ToString
+@Builder(toBuilder = true)
+public class UserRequest {
+
+    @NotBlank(message = "Username is required for registration.")
+    @Size(min = 4, max = 12)
+    private String username;
+
+    @NotEmpty(message = "Password is required for registration.")
+    @Size(min = 8)
+    private String password;
+
+    @NotEmpty(message = "Confirmation of password is required for registration.")
+    @Size(min = 8)
+    private String confirmPassword;
+
+    @NotEmpty(message = "Email is required for registration.")
+    @Email
+    private String email;
+
+    public UserRequest() {}
 }
