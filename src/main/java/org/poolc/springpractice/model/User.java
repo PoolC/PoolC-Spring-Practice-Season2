5c05bcf39afd3aa050879c46856754a6ac5d3319
becooq81 <becooq81@gmail.com>
2023-05-09 17:35:12 +0900

    feat: added user entity

--- src/main/java/org/poolc/springpractice/model/User.java
+++ src/main/java/org/poolc/springpractice/model/User.java
@@ -1,2 +1,44 @@
-package org.poolc.springpractice.model;public class User {
+package org.poolc.springpractice.model;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Getter
+public class User {
+    @Id @GeneratedValue
+    private Long id;
+
+    @NotBlank(message = "Username is required for registration.")
+    @Size(min = 4, max = 12)
+    private String username;
+
+    @NotEmpty(message = "Password is required for registration.")
+    @Size(min = 8)
+    private String password;
+
+    @NotBlank(message = "Name is required for registration.")
+    private String name;
+
+    @NotEmpty(message = "Email is required for registration.")
+    @Email
+    private String email;
+
+    private boolean isMember;
+    private boolean isAdmin;
+
+    public User() {}
+    public User(String username, String password, String name, String email) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
 }
