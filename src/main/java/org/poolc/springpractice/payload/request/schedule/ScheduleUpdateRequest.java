1e6355e5c31370e20d537c3a6a154e8f258ffcff
becooq81 <becooq81@gmail.com>
2023-05-09 19:18:37 +0900

    feat: added schedule update request payload

--- src/main/java/org/poolc/springpractice/payload/request/schedule/ScheduleUpdateRequest.java
+++ src/main/java/org/poolc/springpractice/payload/request/schedule/ScheduleUpdateRequest.java
@@ -4,8 +4,22 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 @Getter
 @ToString
 @Builder(toBuilder = true)
 public class ScheduleUpdateRequest {
+
+    private String title;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private String description;
+
+    public ScheduleUpdateRequest() {}
 }
