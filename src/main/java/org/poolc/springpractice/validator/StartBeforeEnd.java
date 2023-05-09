d14db5b7e01bd8b99550561b6d77ade044392ffa
becooq81 <becooq81@gmail.com>
2023-05-09 19:50:02 +0900

    feat: added start before end validation

--- src/main/java/org/poolc/springpractice/validator/StartBeforeEnd.java
+++ src/main/java/org/poolc/springpractice/validator/StartBeforeEnd.java
@@ -1,2 +1,15 @@
-package org.poolc.springpractice.validator;public interface StartBeforeEnd {
+package org.poolc.springpractice.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = StartBeforeEndValidator.class)
+public @interface StartBeforeEnd {
+    public String message() default "The starting date/time should come before ending date/time.";
+    public Class<?>[] groups() default {};
+    public Class<? extends Payload>[] payload() default {};
 }
