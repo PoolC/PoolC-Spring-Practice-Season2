7947d56aaac52f3b0175d69c467e543c9d9e9a4f
becooq81 <becooq81@gmail.com>
2023-05-09 17:34:25 +0900

    feat: added schedule entity

--- src/main/java/org/poolc/springpractice/model/Schedule.java
+++ src/main/java/org/poolc/springpractice/model/Schedule.java
@@ -1,2 +1,52 @@
-package org.poolc.springpractice.model;public class Schedule {
+package org.poolc.springpractice.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Getter
+public class Schedule {
+    @GeneratedValue @Id
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "calendar_id")
+    @JsonIgnore
+    private Calendar calendar;
+
+    @NotEmpty
+    private LocalDate startDate;
+
+    @NotEmpty
+    private LocalDate endDate;
+
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    private String description;
+
+    public Schedule() {}
+    public Schedule(String title, LocalDate startDate, LocalDate endDate, String description) {
+        this.title = title;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.description = description;
+    }
+    public Schedule(String title, LocalDate startDate, LocalDate endDate, LocalTime startTime, LocalTime endTime, String description) {
+        this.title = title;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.description = description;
+    }
 }
