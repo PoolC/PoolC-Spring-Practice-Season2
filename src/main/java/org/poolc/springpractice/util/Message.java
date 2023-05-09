adb2de690936cc8a10612b6626b5abcde390ccae
becooq81 <becooq81@gmail.com>
2023-05-09 20:33:59 +0900

    feat: added message class for errors

--- src/main/java/org/poolc/springpractice/util/Message.java
+++ src/main/java/org/poolc/springpractice/util/Message.java
@@ -1,2 +1,6 @@
-package org.poolc.springpractice.util;public class Message {
+package org.poolc.springpractice.util;
+
+public class Message {
+
+    public static final String USER_DOES_NOT_EXIST = "The user does not exist.";
 }
