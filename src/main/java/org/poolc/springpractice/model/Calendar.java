da7876203a4e7a01fe4ad0f217c7a6b4f81a1e38
becooq81 <becooq81@gmail.com>
2023-05-09 17:34:45 +0900

    feat: added calendary entity

--- src/main/java/org/poolc/springpractice/model/Calendar.java
+++ src/main/java/org/poolc/springpractice/model/Calendar.java
@@ -1,2 +1,16 @@
-package org.poolc.springpractice.model;public class Calendar {
+package org.poolc.springpractice.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Calendar {
+    @GeneratedValue @Id
+    private Long id;
+    @NotEmpty
+    private String title;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "calendar")
+    private List<Schedule> schedules = new ArrayList<>();
 }
