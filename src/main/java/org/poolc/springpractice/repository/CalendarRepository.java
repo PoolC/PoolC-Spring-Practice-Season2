fc111f73ed0040ff465876331a538bfca12823ad
becooq81 <becooq81@gmail.com>
2023-05-10 19:48:04 +0900

    feat: added method to repository

--- src/main/java/org/poolc/springpractice/repository/CalendarRepository.java
+++ src/main/java/org/poolc/springpractice/repository/CalendarRepository.java
@@ -4,6 +4,9 @@ import org.poolc.springpractice.model.Calendar;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CalendarRepository extends JpaRepository<Calendar, Long> {
+    Optional<Calendar> findByTitle(String title);
 }
