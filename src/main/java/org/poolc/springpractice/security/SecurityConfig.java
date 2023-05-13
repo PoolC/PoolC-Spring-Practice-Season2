beee0581066e5b7dd3c289cf12d6a3633dd3f1f0
becooq81 <becooq81@gmail.com>
2023-05-13 16:50:39 +0900

    feat: security configuration setup

--- src/main/java/org/poolc/springpractice/security/SecurityConfig.java
+++ src/main/java/org/poolc/springpractice/security/SecurityConfig.java
@@ -1,2 +1,12 @@
-package org.poolc.springpractice.security;public class SecurityConfig {
+package org.poolc.springpractice.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
 }
